516fb3a98da848232acd72c25dfe6d5923291f02
Sebastian Roebke <sebastian.roebke@xing.com>
2009-08-25 10:15:41 +0200

    Tests have been verfied to work with sqlite3 (default), mysql and postgresql, which should be enough for now.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,22 +9,16 @@ def load_schema
   ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
   db_adapter = ENV['DB']
   
-  # no db passed, try one of these fine config-free DBs before bombing.
+  # no DB passed, try sqlite3 by default
   db_adapter ||=
     begin
-      require 'rubygems'
-      require 'sqlite'
-        'sqlite'
+      require 'sqlite3'
+        'sqlite3'
     rescue MissingSourceFile 
-      begin
-        require 'sqlite3'
-          'sqlite3'
-      rescue MissingSourceFile 
-      end
     end
     
   if db_adapter.nil? 
-    raise "No DB Adapter selected. Pass the DB= option to pick one, or install Sqlite or Sqlite3."
+    raise "No DB Adapter selected. Configure test/database.yml and use DB=mysql|postgresql|sqlite3 to pick one. sqlite3 will be used by default (gem install sqlite3-ruby)."
   end
   
   ActiveRecord::Base.establish_connection(config[db_adapter])
