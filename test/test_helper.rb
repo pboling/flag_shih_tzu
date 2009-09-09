637b6ec2b1eb2568b956b3d748835b24c1d2cb02
Sebastian Roebke <sebastian.roebke@xing.com>
2009-09-09 16:28:04 +0800

    Reverted disabled loading of rails environment to run the tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] = 'test'
 ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
 
 require 'test/unit'
-#require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb')) 
+require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb')) 
 
 def load_schema
   config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
