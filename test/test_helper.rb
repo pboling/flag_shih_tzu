c745cb6b5e0d49e95e191a60e25d3d58455ed7b3
boosty <sebastian@sebastian-r.de>
2011-10-21 16:13:56 +0200

    Use absolute paths in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,10 +5,10 @@ require "flag_shih_tzu"
 
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
 
-configs = YAML.load_file('test/database.yml')
+configs = YAML.load_file(File.dirname(__FILE__) + "/database.yml")
 ActiveRecord::Base.configurations = configs
 
-db_name = ENV['DB'] || 'sqlite'
+db_name = ENV["DB"] || "sqlite"
 ActiveRecord::Base.establish_connection(db_name)
 
 load(File.dirname(__FILE__) + "/schema.rb")
