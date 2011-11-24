2b9a852cf9ab58762e39188d62f5ac64fd2fee4d
Arturas Slajus <x11@arturaz.net>
2011-11-24 11:36:17 +0200

    Added JRuby support to test suite.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,11 @@ require "flag_shih_tzu"
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
 
 configs = YAML.load_file(File.dirname(__FILE__) + "/database.yml")
+if RUBY_PLATFORM == "java"
+  configs['sqlite']['adapter'] = 'jdbcsqlite3' 
+  configs['mysql']['adapter'] = 'jdbcmysql' 
+  configs['postgresql']['adapter'] = 'jdbcpostgresql' 
+end
 ActiveRecord::Base.configurations = configs
 
 db_name = ENV["DB"] || "sqlite"
