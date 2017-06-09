cf011e2bca5e859e6156da9e36bca6b69f108396
shiro16 <shiro16@pepabo.com>
2017-06-09 15:49:48 +0900

    fixed long line

--- lib/flag_shih_tzu.rb
+++ lib/flag_shih_tzu.rb
@@ -180,7 +180,8 @@ To turn off this warning set check_for_column: false in has_flags definition her
             end
           end
 
-          if ActiveRecord::VERSION::MAJOR >= 5 && ActiveRecord::VERSION::MINOR >= 1
+          if ActiveRecord::VERSION::MAJOR >= 5 &&
+              ActiveRecord::VERSION::MINOR >= 1
             class_eval <<-EVAL, __FILE__, __LINE__ + 1
               def #{flag_name}_saved_changed?
                 if colmn_changes = saved_changes["#{colmn}"]
