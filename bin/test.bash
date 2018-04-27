f28c42d725c46a088f942606468efd14fc0f36ca
Yusuke Ebihara <dev.biibiebi@gmail.com>
2018-04-27 16:38:31 +0900

    support Ruby 2.5.1 and Rails 5.2.0

--- bin/test.bash
+++ bin/test.bash
@@ -64,6 +64,16 @@ do
   run_all_tests_for $rvm_ruby_version $rails_version
 done
 
+# Then run the tests for all versions supported on Ruby 2.5.1
+COMPATIBLE_VERSIONS=(5.0.x 5.1.x 5.2.x)
+Count=0
+while [ "x${COMPATIBLE_VERSIONS[Count]}" != "x" ]
+do
+  rvm_ruby_version=2.5.1
+  rails_version=${COMPATIBLE_VERSIONS[Count]}
+  run_all_tests_for $rvm_ruby_version $rails_version
+done
+
 # Then run the tests for all versions supported on jruby-1.7.26
 #   (which appears to pass for 3.1 - 4.2 inclusive)
 # TODO: Investigate 2 failures on Rails 2.3 and 3.0
